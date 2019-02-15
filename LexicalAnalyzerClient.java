cde1fe3a742779e3b781315c2e02fabba27998cd
qnunnally1 <36525291+qnunnally1@users.noreply.github.com>
2019-02-14 20:11:27 -0500

    Updated comments

--- LexicalAnalyzerClient.java
+++ LexicalAnalyzerClient.java
@@ -2,12 +2,17 @@
 * 
 * @author Grpup 7 (Ryan Walden, Phillip King, Christian Enamorado, Tu Nguyen, Quantonio Nunnally)
 * 
+* LexicalAnalyzer is an object that will take source code as an input and keep a list of lexemes found within the source code. Upon 
+* retrieval of the lexemes kept within the list, a user will be returned any lexemes found and its associated toke identifier.
 **/
 
 import java.io.*;
 
 public class LexicalAnalyzerClient {
 
+	/**
+	* The LexicalAnalyzer object has to be created as an object before use.
+	**/
 	public static void main(String[] args) {
 		//declare and set string to string content of java file passed in
 		String sourceCode = file2Text("Example.java");
@@ -21,7 +26,13 @@ public class LexicalAnalyzerClient {
 		analyzer.getLexemes();
 	}
 	
-	//return string of entire source code file
+	/**
+	* Before any lexemes are found, the source file is needed. To handle this, the client locates the source code file by passing 
+	* its directory as input to a method that returns a string of the complete text within the source code file by use of a 
+	* StringBuilder and BufferedReader. The BufferedReader will read each line of the source code’s file and append this line, as 
+	* a string, to the string to be returned. After this string is returned, the client will use this string as an input to the 
+	* LexicalAnalyzer’s method, buildWord. 
+	**/
 	private static String file2Text(String path) {
 		StringBuilder builder = new StringBuilder();
 		
